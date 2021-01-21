757645d233b840bcb022dd8d8b6261fed18a85bb
victorwikstrom <victor.wikstrom@icloud.com>
2021-01-21 11:17:03 +0100

    Add guess counter. Group programming

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 class Game {
-  private correctNumber: number;
-  private guessNumber: number;
+  private botNumber: number;
+  private userNumber: number;
 
   private botAnswer: string;
   private gameWrapper: HTMLElement;
@@ -9,35 +9,43 @@ class Game {
   private guessButton: HTMLElement;
   private textBox: HTMLElement;
 
+  private guessCountElement: HTMLElement;
+  private guessCount: number;
+
   constructor() {
     this.gameWrapper = document.createElement("div");
     this.gameWrapper.classList.add("game-wrapper");
 
     this.botAnswer = "Start guessing!";
+    this.textBox = document.createElement("span");
+    this.textBox.innerText = this.botAnswer;
 
     this.inputField = document.createElement("input");
     this.guessButton = document.createElement("button");
     this.guessButton.innerHTML = "Gissa";
-    this.textBox = document.createElement("span");
-    this.textBox.innerText = this.botAnswer;
 
-    this.correctNumber = this.getRandomNumber();
-    this.guessNumber = this.getUserInput();
+    this.botNumber = this.getBotNumber();
+    this.userNumber = this.getUserInput();
+
+    this.guessCountElement = document.createElement("span");
+    this.guessCountElement.classList.add("guess-counter");
+    this.guessCount = 0;
+    this.guessCountElement.innerText = String(this.guessCount);
 
     this.gameWrapper.appendChild(this.textBox);
     this.gameWrapper.appendChild(this.inputField);
     this.gameWrapper.appendChild(this.guessButton);
+    this.gameWrapper.appendChild(this.guessCountElement);
   }
 
   public runGame() {
-    console.log(this.correctNumber);
     document.body.appendChild(this.gameWrapper);
     this.guessButton.addEventListener("click", () => {
       this.validateUserInput();
     });
   }
 
-  private getRandomNumber() {
+  private getBotNumber() {
     return Math.floor(Math.random() * Math.floor(20));
   }
 
@@ -46,25 +54,23 @@ class Game {
   }
 
   private validateUserInput() {
-    this.guessNumber = this.getUserInput();
-    console.log(this.guessNumber);
-    if (
-      this.guessNumber > 20 ||
-      this.guessNumber < 0 ||
-      isNaN(this.guessNumber)
-    ) {
+    this.guessCount++;
+    this.guessCountElement.innerText = String(this.guessCount);
+
+    this.userNumber = this.getUserInput();
+
+    if (this.userNumber > 20 || this.userNumber < 0 || isNaN(this.userNumber)) {
       this.botAnswer = "Please choose a number between 1-20";
     } else {
-      this.getBotAnswer(this.guessNumber);
+      this.getBotAnswer(this.userNumber);
     }
-
     this.textBox.innerText = this.botAnswer;
   }
 
   private getBotAnswer(number: number) {
-    if (number > this.correctNumber) {
+    if (number > this.botNumber) {
       this.botAnswer = "Lower!";
-    } else if (number < this.correctNumber) {
+    } else if (number < this.botNumber) {
       this.botAnswer = "Higher!";
     } else {
       this.botAnswer = "Correct!";
