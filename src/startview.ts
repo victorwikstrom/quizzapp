b2196a04c580d0027fe7785361b3a50b649d9246
miaherman <mia.herman@live.se>
2021-01-26 16:43:54 +0100

    en liten logga

--- src/startview.ts
+++ src/startview.ts
@@ -2,14 +2,21 @@ class StartView {
   private gameWrapper: HTMLElement;
   private startGameButton: HTMLElement;
 
+  private gameLogo: HTMLElement;
+
+
   constructor() {
     this.gameWrapper = document.createElement("div");
     this.gameWrapper.classList.add("game-wrapper", "start-view");
+
+    this.gameLogo = document.createElement('div');
+    this.gameLogo.classList.add("logo");
 
     this.startGameButton = document.createElement("button");
     this.startGameButton.classList.add("all-buttons");
 
     this.startGameButton.innerHTML = "START GAME";
+    this.gameWrapper.appendChild(this.gameLogo);
 
     this.gameWrapper.appendChild(this.startGameButton);
   }
@@ -19,6 +26,8 @@ class StartView {
     this.startGameButton.addEventListener("click", () => {
       gameState.updateView("game");
     });
+    
+    
   }
 
   public hide() {
